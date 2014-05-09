ff4da7cf8bd06d0de33213706700f6ce4cb5ff44
Tim Chilvers <tim@Tims-MacBook-Pro.local>
2014-05-09 12:13:30 +0100

    Inline device switching

--- Paper/HACollectionViewSmallLayout.m
+++ Paper/HACollectionViewSmallLayout.m
@@ -15,7 +15,9 @@
     if (!(self = [super init])) return nil;
     
     self.itemSize = CGSizeMake(142, 254);
-    self.sectionInset = UIEdgeInsetsMake((iPhone5 ? 314 : 224), 2, 0, 2);
+    
+    BOOL isIPhone5 = ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO);
+    self.sectionInset = UIEdgeInsetsMake((isIPhone5 ? 314 : 224), 2, 0, 2);
     self.minimumInteritemSpacing = 10.0f;
     self.minimumLineSpacing = 2.0f;
     self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
